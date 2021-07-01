cc98c6936a3694b8e5ca6ef545ceb47d276761cf
obewas <obewas1202@gmail.com>
2021-07-01 15:38:42 +0300

    added styles.css

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Image, Category
 # Create your views here.
+def photo_list(request):
+    photos = Image.objects.all()
+    context = {
+        'photos':photos
+    }
+    return render(request, 'photo_list.html', context)
