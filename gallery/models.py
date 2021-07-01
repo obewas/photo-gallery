874d9e34c83e63e419cacafa17ec4b23ccc3bbc4
obewas <obewas1202@gmail.com>
2021-07-01 15:09:01 +0300

    added base.html and navbar.html

--- gallery/models.py
+++ gallery/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    cats = [
-        ('T', 'Travel'), ('F', 'Family'), ('L', 'Lifestyle'),('S', 'Sports'), 
-    ]
-    name = models.CharField(choices=cats, max_length=100)
+    
+    name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        
         return self.name
 
 class Location(models.Model):
--- gallery/tests/tests_models.py
+++ gallery/tests/tests_models.py
@@ -1,14 +1,28 @@
 from django.test import TestCase
-from ..models import Category
+from ..models import Category, Image, Location
 
 # Create your tests here.
 class CategoryTestClass(TestCase):
     def setUp(self):
-        self.name = Category.objects.create(name=Category.Travel)
+        self.category = Category.objects.create(name='Family')
     
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.name,Category))
+        self.assertTrue(isinstance(self.category,Category))
 
-    def tearDown(self):
-        self.name.dispose()
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location.objects.create(name='Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location,Location))
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.image = Image.objects.create(label='Best', image="shoes.jpg",description='test',
+        category=Category.name, location=Location.name, year_taken='2012-02-12')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
