af55acb3ace0a18c1e83c6d52cbfd10d79211c21
obewas <obewas1202@gmail.com>
2021-07-01 23:39:02 +0300

    created photo_list.html

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,8 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.photo_list, name='photo_list'),
+    url(r'^photo/(\d+)',views.single_photo,name ='photo'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Image, Category
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 # Create your views here.
 def photo_list(request):
     photos = Image.objects.all()
@@ -7,3 +9,11 @@ def photo_list(request):
         'photos':photos
     }
     return render(request, 'photo_list.html', context)
+
+def single_photo(request, photo_id):
+    try:
+       photo =  Image.objects.get(pk=photo_id)
+    except:
+        Image.DoesNotExist
+        raise Http404()
+    return render(request, 'photo.html', {'photo':photo})
