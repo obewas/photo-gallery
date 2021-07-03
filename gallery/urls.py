d903ab0383c50a15ec20f46e0afc5e29f37ca09a
obewas <obewas1202@gmail.com>
2021-07-03 18:46:12 +0300

    created category views

--- gallery/urls.py
+++ gallery/urls.py
@@ -11,6 +11,13 @@ urlpatterns = [
     path('get_photo_by_id/<int:photo_id>',views.get_photo_by_id, name='get_photo_by_id'),
     path('search/', views.search_results, name='search'),
     path('location_filter', views.location_filter, name='location'),
+    path('cats', views.cat, name='cats'),
+    path('family', views.family, name='family'),
+    path('travel', views.travel, name='travel'),
+    path('sports', views.sports, name='sports'),
+    path('landscape', views.landscape, name='landscape'),
+    path('fashion', views.family, name='fashion'),
+    
 
 
 
