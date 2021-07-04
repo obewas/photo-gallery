eeb53c028fed181c330336c21d42d217fbcf241f
obewas <obewas1202@gmail.com>
2021-07-05 01:32:02 +0300

    collected static files

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -124,15 +124,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
